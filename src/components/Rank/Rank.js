3efe76429aac23b1ee071de02a5bfe71ddf79342
TatianaTriukhova <tatiana.triukhova@gmail.com>
2019-04-01 23:28:02 +0300

    front-end

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Rank = () => {
 	return(
 		<div>
-			<div className='white f3'>
+			<div className = 'white f3'>
 			  {'Tatiana, your current rank is...'}
 			</div>
-			<div className='white f1'>
+			<div className = 'white f1'>
 			  {'#5'}
 			</div>
 		</div>
